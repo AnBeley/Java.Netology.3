268c8c3e631dc9266e67d093bf244074764509b1
Anna Markova <anbeley@mail.ru>
2023-05-12 13:52:45 +0300

    Reformated code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
-
-    public int calculate(int price){
+    
+    public int calculate(int price) {
         int mile = 20;
         int bonus = price / mile;
         return bonus;
